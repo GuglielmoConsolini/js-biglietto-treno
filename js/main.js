f488144eca1747553a5a5cf3157f026eec6eac41
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-17 15:58:24 +0200

    Prezzo standard senza sconti

---
+++ js/main.js
@@ -0,0 +1,7 @@
+alert("Calcoliamo il prezzo del tuo biglietto")
+
+const kiloMetri = prompt("Quanti kilometri da percorrere?")
+
+const prezzo = kiloMetri * 0.21;
+
+alert("Prezzo biglietto:" + prezzo);
