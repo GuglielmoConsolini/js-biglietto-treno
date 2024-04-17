c9da31e51431684e1a35c93006d8910711924e78
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-17 16:38:14 +0200

    Prezzo biglietto con scontistica

--- js/main.js
+++ js/main.js
@@ -4,4 +4,33 @@ const kiloMetri = prompt("Quanti kilometri da percorrere?")
 
 const prezzo = kiloMetri * 0.21;
 
-alert("Prezzo biglietto:" + prezzo);
+ console.log(prezzo);
+
+ const età = prompt("Dicci quanti anni hai per ottenere uno sconto");
+
+console.log(età)
+
+let sconto = 1
+
+if(età<18) {
+
+ sconto = 0.80;
+
+
+} else if(età>65) {
+
+ sconto = 0.60;
+
+} else {
+
+sconto = 1
+
+}
+
+console.log(sconto);
+
+let prezzoScontato = prezzo * sconto;
+
+console.log(prezzoScontato);
+
+document.getElementById("price").innerHTML = prezzoScontato
